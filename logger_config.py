989ff3088cacff61b59a15fc10bc4e80700f6ba2
rjpcarvalho <rjp.carvalho@campus.fct.unl.pt>
2025-05-22 10:20:10 +0100

    Atualizar logger_config.py

--- logger_config.py
+++ logger_config.py
@@ -1,17 +1,15 @@
 import logging
 
-# Configuração do logger
-def setup_logger():
-    # Formato do logger
-    log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+LOG_FILE = 'app.log'
+ERROR_FILE = 'stderr.log'
 
-    # Configuração básica
-    logging.basicConfig(
-        level=logging.INFO,
-        format=log_format,
-        handlers=[
-            logging.FileHandler("app.log")  # Apenas ficheiro, sem consola
-        ]
-    )
-
-    return logging.getLogger(__name__)
+# Configura o logger principal
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[
+        logging.FileHandler(LOG_FILE),
+        logging.FileHandler(ERROR_FILE),
+        logging.StreamHandler()
+    ]
+)
